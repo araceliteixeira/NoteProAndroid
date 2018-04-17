82531b4c74a64045c889b13c4b8af65d4d22d66c
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-04-16 20:14:34 -0400

    Implement screen navigation

--- app/src/main/java/com/orion/notepro/view/MainActivity.java
+++ app/src/main/java/com/orion/notepro/view/MainActivity.java
@@ -1,7 +1,9 @@
 package com.orion.notepro.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.orion.notepro.R;
 
@@ -11,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSubjectList(View view) {
+        Intent intent = new Intent(this, SubjectListActivity.class);
+        startActivity(intent);
+    }
+
+    public void createNewNote(View view) {
+        Intent intent = new Intent(this, NoteDetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void showNoteList(View view) {
+        Intent intent = new Intent(this, NoteListActivity.class);
+        startActivity(intent);
     }
 }
