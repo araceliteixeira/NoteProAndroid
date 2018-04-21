95b5ba1913ab8f97e9bad42cf1fa9d3458d54e58
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-04-21 12:28:21 -0400

    Make models implement Serializable

--- app/src/main/java/com/orion/notepro/model/MediaType.java
+++ app/src/main/java/com/orion/notepro/model/MediaType.java
@@ -1,6 +1,8 @@
 package com.orion.notepro.model;
 
-public enum MediaType {
+import java.io.Serializable;
+
+public enum MediaType implements Serializable {
     PHOTO("Photo", 1),
     AUDIO("Audio", 2);
 
