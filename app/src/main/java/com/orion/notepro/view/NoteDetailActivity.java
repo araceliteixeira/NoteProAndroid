f9178237fa7cac027dd86c476e6eb54423d1a93e
Denis Gois <denisdwtg@gmail.com>
2018-04-21 15:39:12 -0400

    First layout for Note Details view

--- app/src/main/java/com/orion/notepro/view/NoteDetailActivity.java
+++ app/src/main/java/com/orion/notepro/view/NoteDetailActivity.java
@@ -1,15 +1,73 @@
 package com.orion.notepro.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.orion.notepro.R;
+import com.orion.notepro.model.Note;
+import com.orion.notepro.model.Subject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NoteDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtNoteTitle)
+    TextInputEditText edtNoteTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+        ButterKnife.bind(this);
+
+        initScreen();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.note_detail_edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.save_note_menu:
+                saveNote();
+                return true;
+            default:
+                Log.w("NotePro", "Menu item not implemented");
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        Log.i("NotePro", edtNoteTitle.getText().toString());
+    }
+
+    private void initScreen() {
+
+//        setUpToolbar(); Comentado para ser usado quando for implementar o note view
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Test");
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+    }
+
+
 }
