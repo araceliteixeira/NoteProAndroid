96a7477d8c0db2d5394e47ed5a22948b648a5359
Denis Gois <denisdwtg@gmail.com>
2018-04-23 20:50:44 -0400

    Take photo and choose one from galery

--- app/src/main/java/com/orion/notepro/view/NoteDetailActivity.java
+++ app/src/main/java/com/orion/notepro/view/NoteDetailActivity.java
@@ -2,13 +2,9 @@ package com.orion.notepro.view;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TextInputEditText;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,21 +12,18 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.google.android.gms.maps.model.LatLng;
+import com.mvc.imagepicker.ImagePicker;
 import com.orion.notepro.R;
 import com.orion.notepro.controller.DatabaseHelper;
 import com.orion.notepro.model.Note;
 import com.orion.notepro.model.Subject;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -59,7 +52,7 @@ public class NoteDetailActivity extends AppCompatActivity {
     }
 
     private void initScreen() {
-        addViewToSlider("Toronto", "https://www.toronto.ca/wp-content/uploads/2018/03/8842-toronto-skyline.jpg");
+        ImagePicker.setMinQuality(600, 600);
 //        setUpToolbar(); Comentado para ser usado quando for implementar o note view
     }
 
@@ -98,9 +91,15 @@ public class NoteDetailActivity extends AppCompatActivity {
         super.onStop();
     }
 
+    @OnClick(R.id.fabPhotos)
+    public void dispatchTakePictureIntent() {
+        ImagePicker.pickImage(this, "Select your image:", REQUEST_TAKE_PHOTO, false);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            mCurrentPhotoPath = ImagePicker.getImagePathFromResult(this, requestCode, resultCode, data);
             Log.i("NotePro", mCurrentPhotoPath);
             addViewToSlider("1 photo of 10", mCurrentPhotoPath);
         }
@@ -139,45 +138,5 @@ public class NoteDetailActivity extends AppCompatActivity {
     public void showMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
-    }
-
-    @OnClick(R.id.fabPhotos)
-    public void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Log.e("NotePro", ex.getMessage());
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
     }
 }
