bee957ef140de597d6a65795614177aaa1f2baf0
Araceli Teixeira <32455451+araceliteixeira@users.noreply.github.com>
2018-04-26 18:52:49 -0400

    Implement add, edit and delete subject

--- app/src/main/java/com/orion/notepro/view/SubjectDetailActivity.java
+++ app/src/main/java/com/orion/notepro/view/SubjectDetailActivity.java
@@ -1,15 +1,99 @@
 package com.orion.notepro.view;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.orion.notepro.R;
+import com.orion.notepro.controller.DatabaseHelper;
+import com.orion.notepro.model.Subject;
+import com.orion.notepro.util.ColorPickerDialog;
 
 public class SubjectDetailActivity extends AppCompatActivity {
+    Subject subject;
+    EditText description;
+    Button color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_detail);
+
+        description = findViewById(R.id.subject_detail_description);
+        color = findViewById(R.id.pick_color);
+
+        subject = (Subject) getIntent().getSerializableExtra("subject");
+
+        if (subject != null) {
+            description.setText(subject.getSubject());
+            color.setBackgroundColor(subject.getColor());
+        }
+    }
+
+    public void showColorPicker(View view) {
+        if (color.getBackground() instanceof ColorDrawable) {
+            ColorDrawable cd = (ColorDrawable) color.getBackground();
+            int colorCode = cd.getColor();
+
+            new ColorPickerDialog(SubjectDetailActivity.this, new UpdateColor(), colorCode).show();
+        }
+    }
+
+    public class UpdateColor implements ColorPickerDialog.OnColorChangedListener {
+        public void colorChanged(int color) {
+            SubjectDetailActivity.this.color.setBackgroundColor(color);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.note_detail_edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.save_note_menu:
+                saveSubject();
+                return true;
+            default:
+                Log.w("NotePro", "Menu item not implemented");
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveSubject() {
+        DatabaseHelper dao = new DatabaseHelper(this);
+
+        if (subject != null) {
+            subject.setSubject(description.getText().toString());
+            if (color.getBackground() instanceof ColorDrawable) {
+                ColorDrawable cd = (ColorDrawable) color.getBackground();
+                int colorCode = cd.getColor();
+                subject.setColor(colorCode);
+            }
+            dao.updateSubject(subject);
+        } else {
+            String text = description.getText().toString();
+            int colorCode = 0;
+            if (color.getBackground() instanceof ColorDrawable) {
+                ColorDrawable cd = (ColorDrawable) color.getBackground();
+                colorCode = cd.getColor();
+            }
+            subject = new Subject(text, colorCode);
+            dao.addSubject(subject);
+        }
+        dao.close();
+        finish();
     }
 }
--- app/src/main/java/com/orion/notepro/view/SubjectListActivity.java
+++ app/src/main/java/com/orion/notepro/view/SubjectListActivity.java
@@ -1,25 +1,34 @@
 package com.orion.notepro.view;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.*;
 import com.orion.notepro.R;
 import com.orion.notepro.controller.DatabaseHelper;
 import com.orion.notepro.controller.SubjectListAdapter;
 import com.orion.notepro.model.Subject;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SubjectListActivity extends AppCompatActivity {
 
-    private ListView subjectList;
+    private SwipeMenuListView subjectList;
     private List<Subject> subjects;
+    private SubjectListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +38,7 @@ public class SubjectListActivity extends AppCompatActivity {
         subjectList = findViewById(R.id.subject_list);
         registerForContextMenu(subjectList);
 
-        DatabaseHelper dao = new DatabaseHelper(this);
-        subjects = dao.selectAllSubjects();
-
-        if(subjects.size() == 0) {
-            dao.addSomeSubjects();
-            subjects = dao.selectAllSubjects();
-        }
-
-        final SubjectListAdapter adapter = new SubjectListAdapter(this, R.layout.activity_subject_list, subjects);
-        subjectList.setAdapter(adapter);
+        loadSubjectList();
 
         subjectList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,5 +50,131 @@ public class SubjectListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        loadSubjectList();
+        super.onResume();
+    }
+
+    private void loadSubjectList() {
+        DatabaseHelper dao = new DatabaseHelper(this);
+        subjects = dao.selectAllSubjects();
+
+        if(subjects.size() == 0) {
+            dao.addSomeSubjects();
+            subjects = dao.selectAllSubjects();
+        }
+
+        adapter = new SubjectListAdapter(this, R.layout.activity_subject_list, subjects);
+        subjectList.setAdapter(adapter);
+
+        configureSwipe();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_button_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_add:
+                Intent intent = new Intent(this, SubjectDetailActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_left:
+                subjectList.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
+                return true;
+            case R.id.action_right:
+                subjectList.setSwipeDirection(SwipeMenuListView.DIRECTION_RIGHT);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void configureSwipe() {
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem editItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                editItem.setBackground(new ColorDrawable(Color.GRAY));
+                // set item width
+                editItem.setWidth(dp2px(90));
+                // set item title
+                editItem.setTitle("Edit");
+                // set item title fontsize
+                editItem.setTitleSize(18);
+                // set item title font color
+                editItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(editItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(dp2px(90));
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        subjectList.setMenuCreator(creator);
+
+        subjectList.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                Subject subject = (Subject) subjectList.getItemAtPosition(position);
+                switch (index) {
+                    case 0:
+                        Intent intent = new Intent(SubjectListActivity.this, SubjectDetailActivity.class);
+                        intent.putExtra("subject", subject);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        deleteSubject(subject);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
+    }
+
+    private void deleteSubject(Subject subject) {
+        final Subject subj = subject;
+        new AlertDialog.Builder(this)
+                .setTitle("Alert")
+                .setMessage("Do you really want to delete this subject?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        DatabaseHelper dao = new DatabaseHelper(SubjectListActivity.this);
+                        dao.deleteSubject(subj);
+                        dao.close();
+                        loadSubjectList();
+                        Toast.makeText(SubjectListActivity.this, "Subject deleted", Toast.LENGTH_SHORT).show();
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
